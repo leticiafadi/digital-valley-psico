ccf161787dc0650745c0c475e7d6ea4a08a1ebf0
Aureliano Neto <aurelianosza@gmail.com>
2019-09-02 13:53:25 -0300

    recuperar senha

---
+++ app/Http/Middleware/Aluno.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+use Illuminate\Support\Facades\Auth;
+
+
+use Closure;
+
+class Aluno
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(!Auth::check())
+            return redirect('/login');
+        if(Auth::user()->tipo != 'aluno')
+            if(Auth::user()->tipo == 'funcionario')
+                return redirect('/home')->withErrors(['nao_permitido' => 'Você não possui acesso a essa área']);
+            else
+                return redirect()->back()->withErrors(['nao_permitido' => 'Você não possui acesso a essa área']);
+
+        return $next($request);
+    }
+}
