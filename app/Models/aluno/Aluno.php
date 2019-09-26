4b58069d6ee4a6037502e4cc0952e26d1db45cf9
Paulo Alencar <paulovictoralencar.13@gmail.com>
2019-09-26 17:44:43 -0300

    Serviço de buscar alunos e  codificação em json

--- app/Models/aluno/Aluno.php
+++ app/Models/aluno/Aluno.php
@@ -4,6 +4,7 @@ namespace App\Models\aluno;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\usuarios\Usuario;
+use Illuminate\Support\Facades\DB;
 
 class Aluno extends Model
 {
@@ -22,8 +23,9 @@ class Aluno extends Model
         return $aluno;
     }
 
+    //busca so os alunos REFATORAR
     public static function buscaAlunos($data,$page){
-        $aluno =  Usuario::where('tipo','aluno')->where('nome_completo','like','%'.$data.'%')->skip(10*$page)->take(10)->get();
+        $aluno = DB::select(DB::raw('select usuario.nome_completo, usuario.id,aluno.matricula,aluno.id,curso.nome FROM aluno, usuario, curso where usuario.tipo = "aluno" and aluno.id_usuario = usuario.id  and curso.id = aluno.id_curso and usuario.nome_completo like "%'.$data.'%"'));
         $tamanho = Usuario::where('tipo','aluno')->where('nome_completo','like','%'.$data.'%')->get()->count();
         $data=[
             'alunos' => $aluno,
@@ -31,6 +33,19 @@ class Aluno extends Model
         ];
         return $data;
     }
+    //busca alunos de acordo com o curso REFATORAR
+    public static function buscaAlunosCurso($data,$page,$id_curso){
+         $aluno = DB::select(DB::raw('select usuario.nome_completo, usuario.id,aluno.matricula,aluno.id,curso.nome FROM aluno, usuario, curso where usuario.tipo = "aluno" and aluno.id_usuario = usuario.id AND aluno.id_curso = '.$id_curso.' and curso.id ='.$id_curso.' and usuario.nome_completo like "%'.$data.'%"'));
+         $tamanho = Usuario::where('tipo','aluno')->where('nome_completo','like','%'.$data.'%')->get()->count();
+         $data=[
+             'alunos' => $aluno,
+             'tamanho' => $tamanho
+         ];
+         return $data;
+     }
 
+
+
+     
 
 }
