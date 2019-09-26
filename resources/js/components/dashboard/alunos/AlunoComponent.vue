2387a298f4e73f8084c2ef4fb102264a5c679b51
Humberto Cavalcante Damasceno <44322360+1berTT@users.noreply.github.com>
2019-09-26 15:49:27 -0300

    COMMIT PRA PODER BAIXAR AS AUTERAÇÕES DO AURELIANO

--- resources/js/components/dashboard/alunos/AlunoComponent.vue
+++ resources/js/components/dashboard/alunos/AlunoComponent.vue
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
     <template>
         <div class="container">
-=======
-<template>
-    <div class="container">
->>>>>>> 104ca74bd3e9d857ec779f9d683a74361840288e
             <vue-snotify></vue-snotify>
             <div class="row">
                 <div class="col col-12">
@@ -14,8 +9,6 @@
                             <i class="fas fa-fw fa-user-graduate"></i>
                                 <span>Visualizar alunos</span>
                             </div>
-<<<<<<< HEAD
-
                             <!--
                             <div class="card-header">
                                 <ul class="nav nav-tabs card-header-tabs">
@@ -31,36 +24,6 @@
                                 </ul>
                             </div>
                             -->
-
-                            
-                            <div class="card-body">
-                                <div class="form-group row">
-                                    <div class="col-md-6">
-                                        <label for="exampleFormControlSelect1">Digite o nome completo do aluno</label>
-                                        <input type="text" class="form-control" name="pesquisa" placeholder="EX: Francisco Etevaldo de Oliveira.">
-                                    </div>
-                                    <div class="col-md-6">
-                                        <label for="exampleFormControlSelect1">Selecione o filtro de pesquisa</label>
-                                        <select class="form-control" id="" :class="{'is-invalid' : errors.has('id_curso')}" v-model="id_curso" name="id_curso">             
-                                            <option value="" selected>Todos os cursos</option>
-                                            <option v-for="curso in cursos" :value="curso.id" :key="curso.id">{{curso.nome}}</option>
-                                        </select>    
-                                    </div>
-                                </div>
-                                
-                                <ul class="list-group">
-                                    <template v-for="aluno in alunos">
-                                        <li class="list-group-item"> <!--v-if="aluno.id != aluno_selecionado"-->
-                                            {{aluno.nome_completo}}
-                                        </li>
-                                    </template>
-                                </ul>
-                                <form class="form" v-on:submit="this.pesquisarAluno">
-                                    <div class="fa fa-pull-right">
-                                        <button class="btn btn-primary btn-lg ml-4 mt-3">Buscar</button>
-                                    </div>
-                                </form>
-=======
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col col-12 col-md-5">
@@ -96,13 +59,11 @@
                                         </li>
                                     </template>
                                 </ul>
->>>>>>> 104ca74bd3e9d857ec779f9d683a74361840288e
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-<<<<<<< HEAD
         </div>
 </template>
 
@@ -112,7 +73,7 @@
     import VeeValidate from 'vee-validate';
     export default {
         props:{
-            baseUrl: String,
+            base_url: String,
             cursos: Array
         },
         data:function(){
@@ -131,52 +92,13 @@
         },
         methods:{
             carregaAlunos: function(){
-                axios.get( this.baseUrl + '/alunos/get').then(response=>{
+                axios.get( this.base_url + '/alunos/get').then(response=>{
                     this.alunos = response.data.alunos;
                 });
-            },
-            carregaCurso : function(curso, index){
-                this.curso_selecionado = curso.id;
-                this.indexCursoSelecionado = index;
             },
         },
         mounted(){
             this.carregaAlunos();
         }
     }
-=======
-    </div>
-</template>
-
-<script>
-import axios    from 'axios';
-import Snotify  from 'vue-snotify';
-import VeeValidate from 'vee-validate';
-
-export default {
-    props:{
-            base_url: String,
-            cursos: Array
-    },
-    data:function(){
-        return{
-            alunos: []
-        }
-    },
-    components:{
-        axios,
-        Snotify
-    },
-    methods:{
-        carregaAlunos: function(){
-            axios.get(this.base_url + '/alunos/get').then(response=>{
-                this.alunos = response.data.alunos;
-            });
-        }
-    },
-    mounted(){
-        this.carregaAlunos();
-    }    
-}
->>>>>>> 104ca74bd3e9d857ec779f9d683a74361840288e
 </script>
