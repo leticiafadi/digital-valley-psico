5ba2c68b12541e308f0754e8ad49e5d0fef03a45
Humberto Cavalcante Damasceno <44322360+1berTT@users.noreply.github.com>
2019-10-03 17:11:44 -0300

    Quebra de componente gerenciar Perfil em sub componentes

---
+++ resources/js/components/dashboard/alunos/perfil/includes/InformacoesAcademicasComponent.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="card-body">
+        <div class="row">
+            <div class="col col-12">
+                <h2>Informações acadêmicas</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col col-4">
+                <div class="form-group">
+                    <label for="matricula">Matricula</label>
+                    <input type="text" class="form-control" name="Matricula" v-model="matricula" disabled="true">
+                </div>
+            </div>
+            <!--<div class="col col-3">
+                <div class="form-group">
+                    <label for="turnoCurso">Turno do curso</label>
+                    <input type="text" class="form-control" v-model="turnoCurso" name="turnoCurso">
+                </div>
+            </div>
+            <div class="col col-3">
+                <div class="form-group">
+                    <label for="formacaoEscolar">Formação escolar básica</label>
+                    <input type="text" class="form-control" v-model="formacaoEscolar" name="formacaoEscolar">
+                </div>
+            </div>-->
+            <div class="col col-4">
+                <div class="form-group">
+                    <label for="curso">Curso atual</label>
+                    <input type="text" class="form-control" v-model="curso" name="curso">
+                </div>
+            </div>
+            <div class="col col-4">
+                <div class="form-group">
+                    <label for="semestreIngresso">Semestre de ingresso</label>
+                    <input type="text" class="form-control" v-model="semestreIngresso" name="semestreIngresso" disabled="true">
+                </div>
+            </div>
+        </div>
+        <!--<div class="row">
+            <div class="col col-4">
+                <div class="form-group">
+                    <label for="semestreAtual">Semestre atual</label>
+                    <input type="text" class="form-control" v-model="semestreAtual" name="semestreAtual">
+                </div>
+            </div>
+            <div class="col col-4">
+                <div class="form-group">
+                    <label for="formacaIngresso">Forma de ingresso</label>
+                    <input type="text" class="form-control" v-model="formaIngresso" name="formaIngresso">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col col-12">
+                <div class="form-group">
+                    <label for="cursosConcluidos">Cursos já concluídos</label>
+                    <input type="text" class="form-control" v-model="cursosConcluidos" name="cursosConcluidos">
+                </div>
+            </div>
+        </div>-->
+    </div>
+</template>
+
+<script>
+import VueCalendar  from 'vuejs-datepicker'
+import {en, ptBR}   from 'vuejs-datepicker/dist/locale'
+export default {
+    components:{
+            VueCalendar,
+            ptBR
+    },
+    data: function(){
+        return {
+            matricula: 0,
+            turnoCurso: '',
+            formacaoEscolar: '',
+            cursoAtual: '',
+            semestreIngresso: '',
+            semestreAtual: '',
+            formaIngresso: '',
+            cursosConcluidos: ''
+        }
+    }
+}
+</script>
