f1b77498e679a2b47b6c0e8deb649286a8540e06
Paulo Alencar <paulovictoralencar.13@gmail.com>
2019-10-21 11:53:16 -0300

    Melhorias no front de acordo com o prototipo

--- resources/views/aluno/includes/sidebar.blade.php
+++ resources/views/aluno/includes/sidebar.blade.php
@@ -1,21 +1,21 @@
-<ul class="sidebar navbar-nav">
+<ul class="sidebar navbar-nav mynav mylink">
     <li class="nav-item">
 		<a class="nav-link" href="{{ route('home') }}">
-			<i class="fas fa-fw fa-home"></i>
+			<i class="fas fa-fw fa-home wi"></i>
 			<span>Gerenciar atendimentos</span>
 		</a>
 	</li>
     
     <li class="nav-item">
 		<a class="nav-link" href="{{ route('marcarAtendimento') }}">
-			<i class="fas fa-fw fa-calendar"></i>
+			<i class="fas fa-fw fa-calendar wi"></i>
 			<span>Marcar atendimento</span>
 		</a>
     </li>
     
     <li class="nav-item">
 		<a class="nav-link" href=" {{route('gerenciarPerfil')}} ">
-			<i class="fas fa-fw fa-user"></i>
+			<i class="fas fa-fw fa-user wi"></i>
 			<span>Gerenciar perfil</span>
 		</a>
 	</li>
--- resources/views/auth/includes/navbar.blade.php
+++ resources/views/auth/includes/navbar.blade.php
@@ -1,12 +1,15 @@
 <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-    <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
+    <div class="container-fluid">
+        <a class="navbar-brand " href="{{ url('/') }}">
             <img src="{{ asset ('imagens/template/icone.png') }}" alt="">
             {{ config('app.name', 'Laravel') }}
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
         </button>
+    </div>
+    <div class="container">
+        
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
