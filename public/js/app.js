225275ae6077fb0b53b6ca754c4aa74c490bfc65
Maria Clara Abreu <mclara.engsoftware@gmail.com>
2019-10-21 16:13:24 -0300

    inserção do hover nos links

--- public/js/app.js
+++ public/js/app.js
@@ -111420,8 +111420,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Cliente\Documents\GitHub\digital-valley-psico\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Cliente\Documents\GitHub\digital-valley-psico\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Maria Clara\Documents\GitHub\digital-valley-psico\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Maria Clara\Documents\GitHub\digital-valley-psico\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
