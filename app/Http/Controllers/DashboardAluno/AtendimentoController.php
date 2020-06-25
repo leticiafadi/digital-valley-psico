b85980c415d7d8320ef295e2dc054b1918230476
Bebeto Alves <bebeto.contato@gmail.com>
2020-06-25 18:59:08 -0300

    Arquivo que faltou no meu commit anterior

--- app/Http/Controllers/DashboardAluno/AtendimentoController.php
+++ app/Http/Controllers/DashboardAluno/AtendimentoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\DashboardAluno;
 
+use App\Models\aluno\Aluno;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Http\Request;
@@ -58,9 +59,76 @@ class AtendimentoController extends Controller
             'i' => "17:00 as 18:00",
             'j' => "18:00 as 19:00",
         ];
-        $atendimento = Atendimento::select('atendimento.id', "usuario.nome_completo as psicologo", 'atendimento.motivo', 'horario_semana.dia', 'horario_semana.horario')->join('funcionario', 'atendimento.id_psicologo', '=', 'funcionario.id')->join('usuario', 'funcionario.id_usuario', '=', 'usuario.id')->join('horario_semana', 'atendimento.id_horario', '=', 'horario_semana.id')->where('id_aluno', '=', Auth::user()->aluno()->id)->get();
+        $atendimento = Atendimento::select('atendimento.id', "usuario.nome_completo as psicologo", 'atendimento.motivo', 'horario_semana.dia', 'horario_semana.horario', 'atendimento.status')->join('funcionario', 'atendimento.id_psicologo', '=', 'funcionario.id')->join('usuario', 'funcionario.id_usuario', '=', 'usuario.id')->join('horario_semana', 'atendimento.id_horario', '=', 'horario_semana.id')->where('id_aluno', '=', Auth::user()->aluno()->id)->get();
         foreach ($atendimento as $atend)
             $atend->horario = $horarios[$atend->horario];
         return response($atendimento, 200)->header('Content-Type', 'text/json');
+    }
+
+    public function cancelarAtendimento(Request $request)
+    {
+        $atendimento = Atendimento::find($request->atid);
+        $aluno = Aluno::select('*')->where('id_usuario', Auth::id())->get();
+
+        if(($atendimento == null) OR ($atendimento->id_aluno != $aluno[0]->id))
+        {
+            //Atendimento não existe ou não pertece a esse aluno
+            return response()->json([
+                'sucesso' => false,
+                'code' => 404,
+                'msg' => 'O atendimento selecionado não existe!',
+            ], 404);
+        }
+
+        if ($atendimento->status == 'ocorrido' OR $atendimento->status == 'cancelado')
+        {
+            //Atendimento já ocorrido, não pode ser cancelado
+            return response()->json([
+                'sucesso' => false,
+                'code' => 403,
+                'msg' => 'Este atendimento não pode ser cancelado!',
+            ], 403);
+        }
+
+        //Cancelar o atendimento
+        $atendimento->status = 'cancelado';
+        $atendimento->save();
+
+        return response()->json([
+            'sucesso' => true,
+            'code' => 200,
+            'msg' => 'Atendimento cancelado com sucesso!',
+        ], 200);
+    }
+
+    public function mostrarDetalhes($id)
+    {
+        $aluno = Aluno::select('*')->where('id_usuario', Auth::id())->get();
+        $atendimento = Atendimento::where('id', $id)
+            ->where('id_aluno', $aluno[0]->id)
+            ->with('aluno.usuario')
+            ->with('observacoes')
+            ->get();
+
+        if ($atendimento->isEmpty())
+        {
+            return response()->json([
+                'sucesso' => false,
+                'code' => 404,
+                'msg' => 'Esse atendimento não foi encontrado!',
+            ], 404);
+        }
+
+        return response()->json([
+            'sucesso' => true,
+            'code' => 200,
+            'msg' => 'Atendimento encontrado com sucesso!',
+            'detalhes' => [
+                'nome_aluno' => $atendimento[0]->aluno->usuario->nome_completo,
+                'matricula' => $atendimento[0]->aluno->matricula,
+                'encaminhamento' => $atendimento[0]->encaminhamento,
+                'observacoes' => $atendimento[0]->observacoes
+            ]
+        ], 200);
     }
 }
